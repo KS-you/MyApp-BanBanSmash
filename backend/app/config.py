a3e8aff180dfb4f96fbaeaecee3414de14a12b48
KikuS <KikuS0211@outlook.jp>
2025-07-06 21:06:22 +0900

    modify ".gitignore","drawio.png" Edit "requirements.txt","config.py","database.py","models.py"

---
+++ backend/app/config.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+
+PASSWORD_SALT = os.getenv("PASSWORD_SALT")
+PASSWORD_PEPPER = os.getenv("PASSWORD_PEPPER")
---
+++ backend/app/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.config import MYSQL_USER, MYSQL_PASSWORD, MYSQL_DB, MYSQL_HOST
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWPRD}@{MYSQ_HOST}/{MYSQL_DB}"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
