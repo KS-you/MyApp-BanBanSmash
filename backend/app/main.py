17cbeceaae6489fbff0cfe7c9d3b8ced8c0979d7
KikuS <KikuS0211@outlook.jp>
2025-07-13 07:46:39 +0900

    edit main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal
 from app import schemas, auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # ローカルのNext.jsを許可
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 def get_db():
     db = SessionLocal()
