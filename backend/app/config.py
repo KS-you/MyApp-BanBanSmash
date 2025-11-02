6a72139201df0ca55b3ce981eab16ab50563d549
KikuS <KikuS0211@outlook.jp>
2025-11-02 23:11:59 +0900

    Edit Deleting unnecessary descriptions

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,3 @@ MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DB = os.getenv("MYSQL_DB")
 MYSQL_HOST = os.getenv("MYSQL_HOST")
-
-PASSWORD_SALT = os.getenv("PASSWORD_SALT")
-PASSWORD_PEPPER = os.getenv("PASSWORD_PEPPER")
