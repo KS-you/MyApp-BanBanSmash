d04347fe9c42c2b19c139f4084129d297b9bef90
KikuS <KikuS0211@outlook.jp>
2025-11-02 23:32:50 +0900

    Edit Deleting unnecessary descriptions

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -73,7 +73,6 @@ def authenticate_user(db: Session, email: str, password: str):
         return None
     return user
 
-# JWT obtainUser
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
         status_code=401,
